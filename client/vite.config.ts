f9ff46b3fc54d9f41db477a87f9962813f877367
Alexbm22 <alexandrub687@gmail.com>
2025-07-10 22:28:53 +0300

    Add support for local HTTPS server using self-signed certificates

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
 import tailwindcss from  '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss(), mkcert()],
+  server: {
+    port:  Number(process.env.VITE_PORT) || 5173,
+    host: process.env.VITE_HOST,
+  },
 })
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,29 @@
 import app from './app';
 import sequelize from './config/database_config';
+import https from 'https';
+import fs from 'fs';
 import { initModels } from './models';
 
 const PORT: number = Number(process.env.PORT) || 5001;
+
+const keyPath = process.env.SSL_KEY_PATH;
+const certPath = process.env.SSL_CERT_PATH;
+
+if (!keyPath || !certPath) {
+  throw new Error("SSL_KEY_PATH and SSL environment variables must be set");
+} 
+
+const sslOptions = {
+  key: fs.readFileSync(keyPath),
+  cert: fs.readFileSync(certPath)
+};
 
 const startServer = async () => {
     try {
         await sequelize.authenticate();
         await initModels();
 
-        app.listen(PORT, () => {
+        https.createServer(sslOptions, app).listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
     } catch (error) {
